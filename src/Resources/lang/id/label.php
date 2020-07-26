c921d1594abd8da45e8383d9ba620e2f1e1aadee
Irvan Maulana <me@tukangremot.com>
2020-07-26 09:07:17 +0700

    fix: label

--- src/Resources/lang/id/label.php
+++ src/Resources/lang/id/label.php
@@ -4,6 +4,7 @@ return [
     'create' => 'Tulis Pesan',
     'time' => 'Waktu',
     'index' => 'Pesan',
+    'list' => 'Daftar Pesan',
     'receiver' => 'Penerima',
     'sender' => 'Pengirim',
     'subject' => 'Subjek',
